78d8be2535328890ea04c4c1d30f3902874c661b
Yousef Malak <y.malak@alhayat.com>
2019-03-20 16:45:34 +0400

    Added the Header component

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+//imports for Material UI
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  appBar: {
+    background: 'transparent',
+    boxShadow: 'none'
+  },
+  typo: {
+    fontSize: '42px'
+  }
+};
+
+function Header(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar className={classes.appBar} position="relative">
+        <Typography className={classes.typo} variant="h1">
+          Watch Your Video
+        </Typography>
+      </AppBar>
+    </div>
+  )
+}
+
+export default withStyles(styles)(Header);
--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -5,6 +5,8 @@ import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 
+//import components
+import Header from './Header'; 
 
 
 const styles = theme => ({
@@ -26,9 +28,9 @@ function MainLayout(props) {
   return (
     <div className={classes.root}>
       <Grid container >
-        <Grid item xs={12}>
+        <Grid item  xs={12}>
           <Paper className={classes.paper}>
-            page Content
+            <Header />
           </Paper>
         </Grid>
       </Grid>
